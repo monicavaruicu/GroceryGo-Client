1a5b508924fb24ad47d9261f7f114c06b5a5f07c
monica-varuicu <varuicu.monica.i6x@student.ucv.ro>
2024-11-19 19:26:44 +0200

    feat: design login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class LoginComponent {
 
+  login() {}
 }
